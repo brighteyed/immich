7877f0fd87e9ad530eae29ce680a6dd1e28f03a9
Jason Rasmussen <jrasm91@gmail.com>
2023-05-15 10:35:20 -0400

    feat: draggable timeline with action bar

--- web/src/routes/(user)/partners/[userId]/+page.svelte
+++ web/src/routes/(user)/partners/[userId]/+page.svelte
@@ -1,25 +1,55 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import { AppRoute } from '$lib/constants';
-	import { goto } from '$app/navigation';
-	import ArrowLeft from 'svelte-material-icons/ArrowLeft.svelte';
-	import ControlAppBar from '$lib/components/shared-components/control-app-bar.svelte';
+	import CircleIconButton from '$lib/components/elements/buttons/circle-icon-button.svelte';
+	import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
 	import AssetGrid from '$lib/components/photos-page/asset-grid.svelte';
+	import ControlAppBar from '$lib/components/shared-components/control-app-bar.svelte';
+	import {
+		assetInteractionStore,
+		isMultiSelectStoreState,
+		selectedAssets
+	} from '$lib/stores/asset-interaction.store';
+	import { locale } from '$lib/stores/preferences.store';
+	import { bulkDownload } from '$lib/utils/asset-utils';
+	import Close from 'svelte-material-icons/Close.svelte';
+	import CloudDownloadOutline from 'svelte-material-icons/CloudDownloadOutline.svelte';
+	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	const handleDownloadFiles = async () => {
+		await bulkDownload('immich', Array.from($selectedAssets), () => {
+			assetInteractionStore.clearMultiselect();
+		});
+	};
 </script>
 
-<main class="grid h-screen pt-[4.25rem] bg-immich-bg dark:bg-immich-dark-bg">
-	<ControlAppBar
-		showBackButton
-		backIcon={ArrowLeft}
-		on:close-button-click={() => goto(AppRoute.SHARING)}
-	>
-		<p slot="leading" class="text-immich-fg dark:text-immich-dark-fg">
-			{data.partner.firstName}
-			{data.partner.lastName}'s photos
-		</p>
-	</ControlAppBar>
+<UserPageLayout
+	user={data.user}
+	title="{data.partner.firstName} {data.partner.lastName}'s Photos"
+	hideNavbar={$isMultiSelectStoreState}
+>
+	<svelte:fragment slot="header">
+		{#if $isMultiSelectStoreState}
+			<ControlAppBar
+				on:close-button-click={() => assetInteractionStore.clearMultiselect()}
+				backIcon={Close}
+				tailwindClasses={'bg-white shadow-md'}
+			>
+				<svelte:fragment slot="leading">
+					<p class="font-medium text-immich-primary dark:text-immich-dark-primary">
+						Selected {$selectedAssets.size.toLocaleString($locale)}
+					</p>
+				</svelte:fragment>
+				<svelte:fragment slot="trailing">
+					<CircleIconButton
+						title="Download"
+						logo={CloudDownloadOutline}
+						on:click={handleDownloadFiles}
+					/>
+				</svelte:fragment>
+			</ControlAppBar>
+		{/if}
+	</svelte:fragment>
 
-	<AssetGrid user={data.partner} />
-</main>
+	<AssetGrid user={data.partner} slot="content" />
+</UserPageLayout>
